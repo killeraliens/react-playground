91108f7986845f916e4c6f9df8027c9f8c0fd65b
killeraliens <killeraliens@outlook.com>
2019-08-07 22:10:44 -0700

    bomb another way

--- src/state/Bomb.js
+++ src/state/Bomb.js
@@ -1,37 +1,67 @@
 import React from 'react';
 
 class Bomb extends React.Component {
+  static defaultProps = {
+    count: 0
+  }
 
+  // constructor(props) {
+  //   super(props);
+
+  //   this.state = {
+  //     noise: 'tick'
+  //   }
+  // }
   constructor(props) {
     super(props);
 
     this.state = {
-      noise: 'tick'
+      count: this.props.count
     }
   }
 
+  // componentDidMount() {
+  //   this.interval = setInterval(() => {
+  //     if (this.state.noise === 'tick') {
+  //       this.setState({ noise: 'tock'})
+  //     } else {
+  //       this.setState({ noise: 'tick'})
+  //     }
+  //   }, 1000);
+
+  //   this.interval = setInterval(() => {
+  //     this.setState({ noise: 'BOOOOOM!'})
+  //   }, 5000);
+  // }
   componentDidMount() {
-    this.interval = setInterval(() => {
-      if (this.state.noise === 'tick') {
-        this.setState({ noise: 'tock'})
-      } else {
-        this.setState({ noise: 'tick'})
-      }
+    let startBomb = setInterval(() => {
+       this.setState({ count: this.state.count + 1})
     }, 1000);
 
-    this.interval = setInterval(() => {
-      this.setState({ noise: 'BOOOOOM!'})
-    }, 5000);
+    this.setState({interval: startBomb});
   }
 
   componentWillUnmount() {
-    clearInterval(this.interval);
+    clearInterval(this.state.interval);
   }
 
+
   render() {
+    console.log(this.state.count);
+    let bombStatus;
+    if (this.state.count % 2 === 0 && this.state.count <= 8) {
+      bombStatus = 'tick';
+    } else if (this.state.count % 2 !== 0 && this.state.count <= 8) {
+      bombStatus = 'tock'
+    } else {
+      bombStatus = 'BOOOM!'
+    }
+    if (this.state.count > 8 ) {
+      clearInterval(this.state.interval);
+    }
     return (
       <div>
-        <p>{this.state.noise}</p>
+        <p>{bombStatus}</p>
       </div>
     )
   }
