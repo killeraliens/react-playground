720295f88d4b1e000b0db7dc226b7c3567a553fe
killeraliens <killeraliens@outlook.com>
2019-10-11 12:33:47 -0700

    messing with protected routes and component destructuring

--- promises.js
+++ promises.js
@@ -1,32 +1,27 @@
 
-function doTask(name) {
-  const p = new Promise((resolve, reject) => {
-    console.log(`${name} has started`);
-    const duration =   Math.floor(Math.random() * 5000);
-    setTimeout(() => {
-      resolve(`${name} has ended after ${duration} milliseconds`);
-    }, duration);
-  });
-  return p;
-}
+// function doTask(name) {
+//   const p = new Promise((resolve, reject) => {
+//     console.log(`${name} has started`);
+//     const duration =   Math.floor(Math.random() * 5000);
+//     setTimeout(() => {
+//       resolve(`${name} has ended after ${duration} milliseconds`);
+//     }, duration);
+//   });
+//   return p;
+// }
 
-// const p1 = doTask('Ali');
-// // console.log(p1);
-// p1.then((result) => {
-//   console.log(result);
-//   return doTask('Brinn');
-// })
-// .then((result) => {console.log(result)});
+// const p1 = doTask('A');
+// const p2 = doTask('B');
+// const p3 = doTask('C');
 
-const p1 = doTask('A');
-const p2 = doTask('B');
-const p3 = doTask('C');
+// Promise.all([p1, p2, p3])
+//   .then((arr) => {
+//     arr.forEach(result => {console.log(result);})
+//     return doTask('D');
+//   })
+//   .then(result => {
+//     console.log(result);
+//   });
 
-Promise.all([p1, p2, p3])
-  .then((arr) => {
-    arr.forEach(result => {console.log(result);})
-    return doTask('D');
-  })
-  .then(result => {
-    console.log(result);
-  });
+
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,74 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Switch, Redirect } from 'react-router-dom';
 import './index.css';
 //import App from './paltrows-power-toes/App';
 //import AppLang from './lang-context/AppLang'
-import ScrapeApp from './scrape-test/ScrapeApp'
+//import ScrapeApp from './scrape-test/ScrapeApp'
 
  //ReactDOM.render(<AppLang />, document.getElementById('root'));
+// ReactDOM.render(
+//   <BrowserRouter >
+//     <ScrapeApp />
+//   </BrowserRouter>,
+//   document.getElementById('root')
+// );
+
+//ReactDOM.render(, document.getElementById('root'));
+
+const Public = (props) => <h3>Public</h3>
+const Protected = (props) => <h3>Protected component. {props.message}</h3>
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div>
+        Login
+      </div>
+    )
+  }
+}
+
+const PrivateRoute = ({component: Component, path: Path, ...restProps}) => {
+  if (restProps.isAuth === true) {
+        return(
+          <Route path={Path} {...restProps} render={(props) => {
+            return <Component message='you made it!!' {...props}/>
+          }}/>
+        )
+  }
+  return <Redirect to='/login' />
+  //this method shows protected path but login component
+}
+
+class SampleApp extends React.Component {
+  state = {
+    auth: true
+  }
+  render() {
+    return(
+      <div>
+        <Link to="/public" >Public</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/protected">Protected</Link>
+        <Switch>
+          <Route path='/public' component={Public}/>
+          <Route path='/login' component={Login}/>
+          <PrivateRoute isAuth={this.state.auth} path='/protected' component={Protected}/>
+         {/* <PrivateRoute isAuth={this.state.auth} path='/protected' render={(props) => {
+            <Protected message='you made it!!' {...props}/>
+          }}/>*/}
+
+        </Switch>
+      </div>
+    )
+
+  }
+}
+
 ReactDOM.render(
-  <BrowserRouter >
-    <ScrapeApp />
+  <BrowserRouter>
+    <SampleApp />
   </BrowserRouter>,
   document.getElementById('root')
 );
-
-//ReactDOM.render(, document.getElementById('root'));
