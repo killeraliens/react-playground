e1549de890f9c8417b7777743a6e537958ac2364
killeraliens <killeraliens@outlook.com>
2019-10-12 13:43:10 -0700

    needs server side reqs

--- src/scrape-test/Login.js
+++ src/scrape-test/Login.js
@@ -2,15 +2,30 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Login extends Component {
+  state = {
+    username: '',
+    password: ''
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('logging in with', this.state)
+  }
+
+  updateState = (e) => {
+    const {name, value} = e.target
+    let newState = { ...this.state, [name]: value}
+     this.setState(newState)
+    //return this.setState(newState)
+  }
 
   render() {
     return(
-      <form className="Login" onSubmit={() => {}} >
+      <form className="Login" onSubmit={this.handleSubmit} >
         <h2>Login</h2>
-        <label for="username">Username</label>
-        <input id="username" name="username" type="text" />
-        <label for="password">Password</label>
-        <input id="password" name="password" type="text" />
+        <label htmlFor="username">Username</label>
+        <input id="username" name="username" type="text" value={this.state.username} onChange={this.updateState}/>
+        <label htmlFor="password">Password</label>
+        <input id="password" name="password" type="text" value={this.state.password} onChange={this.updateState}/>
         <button type="submit">Submit</button>
       </form>
     )
