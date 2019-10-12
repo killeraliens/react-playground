eaccc84b9218fba910232dc8970f520e0bdf39f1
killeraliens <killeraliens@outlook.com>
2019-10-11 21:08:52 -0700

    removed cheerio and set for server fb calls

--- src/scrape-test/UrlForm.js
+++ src/scrape-test/UrlForm.js
@@ -1,97 +1,49 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
-import cheerio from 'cheerio';
-//import $ from 'jquery'
 
 class UrlForm extends Component {
+  static defaultProps = {
+    onAddEvent: () => {},
+  }
 
   state = {
-    urlVal: '',
-    eventObj: {},
-    venueObj: {},
+    inputVal: '',
     loading: false
   }
 
-
-  updateUrlVal = (e) => {
+  updateInputVal = (e) => {
     this.setState({
-      urlVal: e.target.value,
-      event: {}
+      inputVal: e.target.value
     })
   }
 
-  // makeEventWithCheerio = ($) => {
-  //   const eventObj = { ...this.state.event }
-  //   console.log($('body').html())
-  //   if ($('#seo_h1_tag')) eventObj.title = $('#seo_h1_tag').text();
-
-  //   this.setState({
-  //     event: eventObj
-  //   })
-  //   //my event object needs to reference a venue id or create a new venue object
-  // }
-
-
-  // handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   const corsMed = 'https://cors-anywhere.herokuapp.com/';
-  //   const url = this.state.urlVal;
-  //   //const url = 'https://www.facebook.com/events/2355225007893472'
-
-  //   fetch(corsMed + url)
-  //   .then(res => {
-  //     if(!res.ok) {
-  //       throw new Error({res})
-  //     }
-  //     return res.text()
-  //   })
-  //   .then(res => {
-  //     this.setState({
-  //       urlVal: ''
-  //     });
-  //     console.log(res)
-  //     let $ = cheerio.load(res);
-  //     this.makeEventWithCheerio($);
-  //     this.setState({
-  //       loading: true
-  //     })
-  //   })
-  //   .then(() => {
-  //       // I need to check the accuracy of the event object here and throw
-  //       // an error if the data is not right or they need to re-enter the url
-  //       // how is best to handle form errors? (will need more for custom form)
-  //       const newEvent = { ...this.state.event }
-  //       this.setState({
-  //         loading: false,
-  //         event: {}
-  //       })
-  //       this.props.onAddEvent(newEvent);
-  //       this.props.history.push('/');
-  //   })
-  //   .catch(err => {
-  //     console.log('ERROR REASON:', err)
-  //     this.props.history.push('/add-event');
-  //     //this.props.passErrorStatusText(err)
-  //     // how do I use context to set a global App error with the message (so that ScrapeApp renders correct page)?
-  //   })
-  // }
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    fetch('localhost:5000/express_backend')
+  callFacebookApi = (eventId) => {
+    //what url do i fetch to send the eventId to search?
+    fetch('localhost:5000/auth/facebook/callback')
     .then(res => {
       if (!res.ok) {
         throw new Error(res)
       }
-      res.json()
+      return res.json()
     })
     .then(resJson => {
-      //set state
+      //const newEvent = resJson
+      //this.context.addEvent(newEvent)
+      //this.props.onAddEvent(newEvent)
     })
     .catch(err => {
-      console.log('fetch related error use context', err)
+      console.log('trouble finding event', err)
     })
-    //console.log('call api and make object next');
+
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const stateCopy  = { ...this.state }
+    const eventId = stateCopy.inputVal
+    // should I also be sending the current users login credentials here?
+    // this.callFacebookApi(eventId, contextUser)
+    this.callFacebookApi(eventId)
   }
 
   render() {
@@ -101,8 +53,8 @@ class UrlForm extends Component {
 
     return(
       <form className="UrlForm" onSubmit={this.handleSubmit}>
-        <label htmlFor="url-input">Type in URL</label>
-        <input id="url-input" name="url-input" type="text" value={this.state.urlVal} onChange={this.updateUrlVal} required/>
+        <label htmlFor="url-input">Type in Event Url or Id</label>
+        <input id="url-input" name="url-input" type="text" value={this.state.inputVal} onChange={this.updateInputVal} required/>
         <button type="submit">Scrape Dis</button>
       </form>
     )
