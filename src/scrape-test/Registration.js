c72996649b3cbca2fcfa24962c592f4c12489c6b
killeraliens <killeraliens@outlook.com>
2019-10-13 14:56:05 -0700

    validation errors

--- src/scrape-test/Registration.js
+++ src/scrape-test/Registration.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ValidationError from './ValidationError';
 
 class Registration extends Component {
 
@@ -6,35 +7,33 @@ class Registration extends Component {
     super(props);
 
     this.state = {
-      name: { value: '' },
-      email: { value: '' },
-      password: { value: '' },
-      repeatPassword: { value: '' }
+      name: { value: '', touched: false },
+      email: { value: '', touched: false },
+      password: { value: '', touched: false },
+      repeatPassword: { value: '', touched: false }
+
     }
   }
 
   handleSubmit = (e) => {
     e.preventDefault();
 
-   this.validateName()
-   this.validateEmail()
-   this.validatePassword()
-   this.validateRepeatPassword()
-    console.log(this.validateName())
-    console.log(this.validateEmail())
-    console.log(this.validatePassword())
-    console.log(this.validateRepeatPassword())
+   // this.validateName()
+   // this.validateEmail()
+   // this.validatePassword()
+   // this.validateRepeatPassword()
+
   }
 
   updateValue = (e) => {
     const { name, value } = e.target;
     this.setState({
-      [name]: value
+      [name]: {value, touched: true}
     });
   }
 
   validateName = () => {
-    const name = this.state.name.trim();
+    const name = this.state.name.value.trim();
     return name.length === 0
       ? 'name required'
       : name.length < 3 || name.length > 12
@@ -43,7 +42,7 @@ class Registration extends Component {
   }
 
   validateEmail = () => {
-    const email = this.state.email.trim();
+    const email = this.state.email.value.trim();
     return email.length === 0
       ? 'email required'
       : !(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))
@@ -52,7 +51,7 @@ class Registration extends Component {
   }
 
   validatePassword = () => {
-    const password = this.state.password.trim();
+    const password = this.state.password.value.trim();
     return password.length === 0
       ? 'password required'
       : password.length < 5 || password.length > 12
@@ -63,8 +62,8 @@ class Registration extends Component {
   }
 
   validateRepeatPassword = () => {
-    const password = this.state.password.trim();
-    const repeatPassword = this.state.repeatPassword.trim();
+    const password = this.state.password.value.trim();
+    const repeatPassword = this.state.repeatPassword.value.trim();
     return repeatPassword.length === 0
       ? 'enter password a second time'
       : repeatPassword !== password
@@ -79,18 +78,22 @@ class Registration extends Component {
         <div className="form-group">
           <label htmlFor="name">Name*</label>
           <input type="text" id="name" name="name" onChange={this.updateValue}/>
+          <ValidationError message={this.validateName()} />
         </div>
         <div className="form-group">
           <label htmlFor="email">Email*</label>
           <input type="email" id="email" name="email" onChange={this.updateValue}/>
+          <ValidationError message={this.validateEmail()} />
         </div>
         <div className="form-group">
           <label htmlFor="password">Password*</label>
           <input type="password" id="password" name="password" onChange={this.updateValue}/>
+          <ValidationError message={this.validatePassword()} />
         </div>
         <div className="form-group">
           <label htmlFor="repeat-password">Repeat Password*</label>
-          <input type="password" id="repeaPassword" name="repeatPassword" onChange={this.updateValue}/>
+          <input type="password" id="repeatPassword" name="repeatPassword" onChange={this.updateValue}/>
+          <ValidationError message={this.validateRepeatPassword()} />
         </div>
         <button type="reset" onClick={() => this.props.history.push('/')}>Cancel</button>
         <button type="submit">Ok Done</button>
